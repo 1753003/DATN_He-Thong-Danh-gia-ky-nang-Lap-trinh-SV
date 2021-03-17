1e81d79e9248fe17124ae05b90ef803ee010a63b
PHÙ VĨNH HÙNG <phuvinhhung1999@gmail.com>
2021-03-17 13:08:29 +0700

    Revert "Add Page Report"
    This reverts commit fc679f82384bae5531ac274a0586dcd76702a3ed.

--- Frontend/src/pages/Creator/ReportDetail/Cotents/Feedback/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './index.less';
-
-const Feedback = () => {
-  return <div className={styles.container}>Feedback</div>;
-};
-
-export default Feedback;
--- Frontend/src/pages/Creator/ReportDetail/Cotents/Questions/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './index.less';
-
-const Questions = () => {
-  return <div className={styles.container}>Questions</div>;
-};
-
-export default Questions;
