31e5ea9ca9ff94d24d584d7fcf8617b8c3ceaf06
PHÙ VĨNH HÙNG <phuvinhhung1999@gmail.com>
2021-03-17 13:17:17 +0700

    Revert "Revert "Add Page Report""
    This reverts commit 36badc9f330dc6dffa14c6853de7e547e9b1413b.

---
+++ Frontend/src/pages/Creator/ReportDetail/Cotents/Questions/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.less';
+
+const Questions = () => {
+  return <div className={styles.container}>Questions</div>;
+};
+
+export default Questions;
