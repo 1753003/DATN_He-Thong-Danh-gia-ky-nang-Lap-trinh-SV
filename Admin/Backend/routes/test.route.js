133a714bdc737c24f45e851c0090763d5ece4b84
Khánh Duy <pupipinpon@gmail.com>
2021-04-09 16:15:01 +0700

    setup

--- Admin/Backend/routes/test.route.js
+++ Admin/Backend/routes/test.route.js
@@ -2,6 +2,24 @@ const express = require('express');
 const router = express.Router();
 const testModel = require('../models/test.model')
 
+var firebase_realtime = require("firebase/app");
+require("firebase/database");
+var config = {
+    apiKey: "AIzaSyC_FKi-svb2idZpvqsfPFWASeHUS60O9eU",
+    authDomain: "devcheckpro.firebaseapp.com",
+    databaseURL: "https://devcheckpro-default-rtdb.firebaseio.com",
+    storageBucket: "devcheckpro.appspot.com"
+};
+
+firebase_realtime.initializeApp(config);
+
+function writeNewNotification(uid, testID, check) {
+    firebase_realtime.database().ref('notifications/'+uid).set({
+        testID: testID,
+        check: check
+    })
+}
+
 router.get('/invalid', async function (req, res) {
    const list = await testModel.getTestListInValid()
    res.json(list);
@@ -60,6 +78,8 @@ router.get('/sendemail/:id', async function (req, res){
 router.post('/setvalid/:id', async function (req, res){
     const id = req.params.id;
     await testModel.setValid(id);
+
+    writeNewNotification('zcwVw4Rjp7b0lRmVZQt6ZXmspql1', id, true);
     res.json(await testModel.getAll());
 })
 
