ea189800eda88d8db8a20d32297b4df6a1a8b4eb
1753003 <56356560+1753003@users.noreply.github.com>
2021-05-13 05:50:59 +0000

    Update frontend to output generated at 8422e61

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,9 +1,9 @@
 const Constant = {
   CORS:
-    process.env.NODE_ENV === 'production' ? 'https://devcheckpro.web.app' : 'http://localhost:8000',
+    process.env.NODE_ENV === 'production' ? 'https://codejoy-fe.herokuapp.com/' : 'http://localhost:8000',
   API:
     process.env.NODE_ENV === 'production'
-      ? 'https://codejoy.herokuapp.com'
+      ? 'https://codejoy-be.herokuapp.com/'
       : 'http://localhost:5000',
 };
 
