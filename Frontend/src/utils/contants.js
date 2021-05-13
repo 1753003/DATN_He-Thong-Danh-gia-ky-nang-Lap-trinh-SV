f8e4dc8994f13eb1f1a4d3774acc4c1785d51859
Nguyen Pham Quang DInh <chromevi123@gmail.com>
2021-05-13 13:54:23 +0700

    edit

--- Frontend/src/utils/contants.js
+++ Frontend/src/utils/contants.js
@@ -1,9 +1,9 @@
 const Constant = {
   CORS:
-    process.env.NODE_ENV === 'production' ? 'https://codejoy-fe.herokuapp.com/' : 'http://localhost:8000',
+    process.env.NODE_ENV === 'production' ? 'https://codejoy-fe.herokuapp.com' : 'http://localhost:8000',
   API:
     process.env.NODE_ENV === 'production'
-      ? 'https://codejoy-be.herokuapp.com/'
+      ? 'https://codejoy-be.herokuapp.com'
       : 'http://localhost:5000',
 };
 
