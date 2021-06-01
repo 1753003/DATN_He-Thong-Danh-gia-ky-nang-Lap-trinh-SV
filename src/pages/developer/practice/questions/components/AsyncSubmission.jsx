e70a0469630f927246af01ea86d86f562318e135
1753003 <56356560+1753003@users.noreply.github.com>
2021-06-01 08:00:47 +0000

    Update frontend to output generated at 3eb70c7

--- src/pages/developer/practice/questions/components/AsyncSubmission.jsx
+++ src/pages/developer/practice/questions/components/AsyncSubmission.jsx
@@ -1,8 +1,10 @@
 import { dynamic } from 'umi';
+import Loading from '@/components/PageLoading'
 export default dynamic({
   loader: async function() {
     // webpackChunkName tells webpack create separate bundle for HugeA
     const { default: AsyncSubmission } = await import(/* webpackChunkName: "external_A" */ '@/components/Submission');
     return AsyncSubmission;
   },
+  loading: Loading,
 });
