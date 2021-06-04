4c39789d65a725b6c96c5a3308164555bb9b7c7b
1753003 <56356560+1753003@users.noreply.github.com>
2021-06-04 12:05:30 +0000

    Update frontend to output generated at 75cc0a2

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -3,7 +3,7 @@ const Constant = {
     process.env.NODE_ENV === 'production' ? 'https://codejoy-fe.herokuapp.com' : 'http://localhost:8000',
   API:
     process.env.NODE_ENV === 'production'
-      ? 'http://3.0.40.66:5000'
+      ? 'https://codejoy-fe.herokuapp.com'
       : 'http://localhost:5000',
 };
 
