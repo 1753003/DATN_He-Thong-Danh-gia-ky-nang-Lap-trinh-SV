349c8cc9e21a8baf65ff7837aa611350bb97d8b7
1753003 <56356560+1753003@users.noreply.github.com>
2021-07-16 16:14:59 +0000

    Update frontend to output generated at ef13358

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ const Constant = {
     process.env.NODE_ENV === 'production' ? 'https://codejoyfe.me' : 'http://localhost:8000',
   API:
     process.env.NODE_ENV === 'production'
-      ? 'https://codejoyfe.me'
+      ? 'https://be.codejoyfe.me'
       : 'http://localhost:5000',
   JUDGE: process.env.NODE_ENV === 'production'
   ? 'https://codejoy-fe.herokuapp.com'
