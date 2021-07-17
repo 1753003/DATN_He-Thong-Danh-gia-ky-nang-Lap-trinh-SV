5d21a16af9d505ac1653e95a4f72b7d9442fe080
1753003 <56356560+1753003@users.noreply.github.com>
2021-07-17 14:39:52 +0000

    Update frontend to output generated at 7a5808e

--- src/services/user.js
+++ src/services/user.js
@@ -1,5 +1,8 @@
 import Constant from '@/utils/constants';
 import request from '@/utils/request';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import tokenHandling from './tokenHandling';
 export async function query() {
   return request(`${Constant.API}/api/users`);
 }
@@ -8,4 +11,63 @@ export async function queryCurrent() {
 }
 export async function queryNotices() {
   return null;
+}
+export function queryInviteList(id) {
+  return [
+    {
+      testName:"Multiple Variables",
+      testID: 1,
+      author: "Mr.A"
+    },
+    {
+      testName:"Multhahahhaaiple Variables",
+      testID: 2,
+      author: "Mr.A"
+    },
+    {
+      testName:"i dunno Variables",
+      testID: 3,
+      author: "Mr.A"
+    },
+    {
+      testName:"you're tirght Variables",
+      testID: 4,
+      author: "Mr.A"
+    },
+    {
+      testName:"DOntkey Variables",
+      testID: 5,
+      author: "Mr.A"
+    },
+    {
+      testName:"Nerve Variables",
+      testID: 6,
+      author: "Mr.A"
+    },
+    {
+      testName:"Random Variables",
+      testID: 7,
+      author: "Mr.A"
+    },
+  ]
+  // return new Promise((resolve, reject) => {
+  //   var options = {
+  //     method: 'GET',
+  //     withCredentials: true,
+  //     url: `${Constant.API}/api/practice/${id}`,
+  //     headers: {
+  //       'access-control-allow-origin': Constant.CORS,
+  //       accessToken: Cookies.get('accessToken'),
+  //     },
+  //   };
+  //   axios
+  //     .request(options)
+  //     .then((response) => {
+  //       resolve(response.data);
+  //     })
+  //     .catch((error) => {
+  //       const message = error.response.data.message;
+  //       tokenHandling(message, resolve, options);
+  //     });
+  // });
 }
