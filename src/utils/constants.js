5e119c9262f14c4dcefca738b53507a6cfd23d41
1753003 <56356560+1753003@users.noreply.github.com>
2021-07-28 17:57:50 +0000

    Update frontend to output generated at 019df9e

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,7 @@
 const Constant = {
   CORS: process.env.NODE_ENV === 'production' ? 'https://codejoyfe.me' : 'http://localhost:8000',
   API: process.env.NODE_ENV === 'production' ? 'https://be.codejoyfe.me' : 'http://localhost:5000',
-  JUDGE:
-    process.env.NODE_ENV === 'production' ? 'https://be.codejoyfe.me' : 'https://be.codejoyfe.me',
+  JUDGE: "https://codejoy-fe.herokuapp.com",
   MIN_SCREEN_WIDTH: 768,
 };
 
