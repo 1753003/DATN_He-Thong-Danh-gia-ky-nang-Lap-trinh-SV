0c3fc2f2b915d8f875b0aaa695191add1813ad99
SoLoPu <40622330+SoLoPu@users.noreply.github.com>
2021-07-28 09:14:50 +0000

    Update frontend to output generated at 3e97681

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,14 @@
 const Constant = {
-  CORS:
-    process.env.NODE_ENV === 'production' ? 'https://codejoyfe.me' : 'http://localhost:8000',
+  CORS: process.env.NODE_ENV === 'production' ? 'https://codejoyfe.me' : 'http://localhost:8000',
   API:
     process.env.NODE_ENV === 'production'
       ? 'https://codejoy-be.herokuapp.com'
       : 'http://localhost:5000',
-  JUDGE: process.env.NODE_ENV === 'production'
-  ? 'https://codejoy-fe.herokuapp.com'
-  : 'https://codejoy-fe.herokuapp.com',
+  JUDGE:
+    process.env.NODE_ENV === 'production'
+      ? 'https://codejoy-fe.herokuapp.com'
+      : 'https://codejoy-fe.herokuapp.com',
+  MIN_SCREEN_WIDTH: 768,
 };
 
 export default Constant;
