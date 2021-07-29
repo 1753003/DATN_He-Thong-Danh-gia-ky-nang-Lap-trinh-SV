cebbd17447f0603e5bce1c90ce027d2176fd7989
1753048 <56597931+1753048@users.noreply.github.com>
2021-07-29 09:26:52 +0000

    Update backend to output generated at 868adf9

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use("/api/auth", require("./routes/auth.route"));
 app.use("/api/token", auth, require("./routes/token.route"));
 app.use("/api/creator", auth, require("./routes/creator.route"));
 app.use("/api/practice", auth, require("./routes/practice.route"));
-app.use("/api/test", require("./routes/test.route"));
+app.use("/api/test", auth, require("./routes/test.route"));
 app.use("/api/submissions", auth, require("./routes/submissions.route"));
 app.use("/api/search", auth, require("./routes/search.route"));
 app.use("/api/developer", auth, require("./routes/developer.route"));
